c0d1bb2dbc117745b53201bf7fcfd967081ad4bd
tuyaqi12306 <1055126409@qq.com>
2019-12-04 17:20:26 +0800

    基本功能及其优化

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import io from 'socket.io-client';
 import PixelGrid from './PixelGrid'
 import ColorSelect from './ColorSelect'
+import { produce } from 'immer'
 import './App.css';
 
 class App extends React.Component{
@@ -16,10 +17,14 @@ class App extends React.Component{
     // 获取数据 1.执行此生命周期函数dom已经生成好，可以操作dom
     this.socket = io('ws://localhost:3005/') //与服务器建立连接
     this.socket.on('pixel-data', (data) => {
-      console.log(data)
       this.setState({
         pixelData: data
       })
+    })
+    this.socket.on('updata-dot', info => {
+      this.setState(produce(this.state, state => {
+        state.pixelData[info.row][info.col] = info.color
+      }))
     })
   }
   handlePixelClick = (row, col) => {
--- src/PixelGrid.js
+++ src/PixelGrid.js
@@ -1,27 +1,35 @@
-import React from 'react'
-function PixelGrid(props) {
-  if (!props.pixels) {
-    return null
-  }else {
-    return (
-      <table style={{tableLayout: 'fixed'}}>
-        <tbody>
-          {
-            props.pixels.map((row,rowIdx) => (
-              <tr key={rowIdx}>
-                {row.map((color, colIdx) => (
-                  <td
-                    key={colIdx}
-                    onClick={() => props.onPixelClick(rowIdx, colIdx)}
-                    style={{width: '5px', height: '5px',backgroundColor: color}}
-                  ></td>
-                ))}
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
-    )
+import React, { PureComponent } from 'react'
+class PixelGrid extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+  handleDotClick = (rowIdx, colIdx) => {
+    this.props.onPixelClick(rowIdx, colIdx)
+  }
+  render() {
+    if (!this.props.pixels) {
+      return null
+    }else {
+      return (
+        <table style={{tableLayout: 'fixed'}}>
+          <tbody>
+            {
+              this.props.pixels.map((row,rowIdx) => (
+                <tr key={rowIdx}>
+                  {row.map((color, colIdx) => (
+                    <td
+                      key={colIdx}
+                      onClick={() => this.handleDotClick(rowIdx, colIdx)}
+                      style={{width: '5px', height: '5px',backgroundColor: color}}
+                    ></td>
+                  ))}
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      )
+    }
   }
 }
 
